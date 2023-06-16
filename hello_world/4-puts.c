02a6c79bd89b21d4c71dffb37a42e1ec5d58a1f7
jnieves2177 <jnieves3177@gmail.com>
2023-06-16 17:51:16 +0000

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,23 @@
+
+
+/*
+ * File: 4-puts.c
+ * Auth: Jose Nieves <6845@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
+
