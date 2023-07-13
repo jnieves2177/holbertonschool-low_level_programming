1508f3c4a98c90ccc037c91fc00fff2124c749e1
jnieves2177 <jnieves3177@gmail.com>
2023-07-13 17:15:20 +0000

    malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Jose N.
+ * malloc_checked - allocates memory checks for failure
+ *
+ * @b: the size in bytes to allocate
+ *
+ * Return: exits with 98 on fail. ptr  memory on success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ar;
+
+	ar = malloc(b);
+	if (ar == NULL)
+		exit(98);
+	return (ar);
+}
