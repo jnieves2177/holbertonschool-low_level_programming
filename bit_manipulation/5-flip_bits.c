27b47ef1daf8050aae60b39dd2368d81a1cb5e6e
jnieves2177 <jnieves3177@gmail.com>
2023-08-01 20:55:32 +0000

    bit

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,20 +1,25 @@
+#include "main.h"
+
 /**
- * flip_bits - A function that gets number of bits needed to flip
- * @n: How many bit flips are needed to equal m for n
- * @m: The number to set other equal
- * Return: The number of fliped bits
+ * flip_bits - determine how many bits to flip to get from one num to another
+ * @n: number
+ * @m: number2
+ * Return: how many bits differ
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flips = 0;
-	unsigned long int xor = (n ^ m);
-	unsigned long int max = 0x01;
+	unsigned int diff_bits = 0;
+	unsigned long int difference;
 
-	while (max <= xor)
-	{
-		if (max & xor)
-			flips++;
-		max <<= 1;
-	}
-	return (flips);
+	/* Xor both nums to show bit 1 if different bits */
+	difference = n ^ m;
+
+	/* keep shifting difference to right and tallying the ones up */
+	do {
+		diff_bits += (difference & 1);
+		difference >>= 1;
+	} while
+		(difference > 0);
+
+	return (diff_bits);
 }
